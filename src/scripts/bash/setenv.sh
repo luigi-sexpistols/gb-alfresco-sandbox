182edc0098aacf5acaa57c8e969b7c447e335fcf
Ashley richmond <ashley@richmond.cool>
2025-03-12 09:27:17 +1000

    Removed bash scripts.

--- src/scripts/bash/setenv.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export GB_TF_AWS_MFA_DEVICE_ARN='arn:aws:iam::800891318996:mfa/Personal-Phone'
-export GB_TF_AWS_ASSUMED_ROLE_ARN='arn:aws:iam::202533530829:role/AdminRole'
-export GB_TF_AWS_CLI_PROFILE_IDENTITY='gb-identity'
-export GB_TF_AWS_CLI_PROFILE_ASSUMED_ROLE='terraform'
-export GB_TF_AWS_REGION='ap-southeast-2'
